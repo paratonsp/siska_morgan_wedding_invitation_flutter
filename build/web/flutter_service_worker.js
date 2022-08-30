6f15fea0a9bd3f1b5f635af7f40d44563050a03c
Pesoros <pesoros@Paratons-MacBook-Pro.local>
2022-08-31 00:43:08 +0700

    web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "754d05a861b8f4051055bb48c5372749",
-"index.html": "8beb42176f6e80fa27bdf7abbc4e39d7",
-"/": "8beb42176f6e80fa27bdf7abbc4e39d7",
-"main.dart.js": "8eff20868d9b51c436251d73589ca7e8",
+"index.html": "01db26e7dc82756de9e638fae510a599",
+"/": "01db26e7dc82756de9e638fae510a599",
+"main.dart.js": "f80bb8fe84a470322701f2c238cd9af7",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "6db3c9b111b8734532ce3988b4757433",
 "icons/Icon-192.png": "6db3c9b111b8734532ce3988b4757433",
